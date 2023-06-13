507613b54d29bddaee029beb86c8c401ee4b795b
Angus Gibson <angus@agibson.me>
2023-06-13 15:14:27 +1000

    Require formatting to pass before tests

--- mom6_regional/mom6_regional.py
+++ mom6_regional/mom6_regional.py
@@ -2,5 +2,6 @@ __all__ = [
     "parabola",
 ]
 
+
 def parabola(x):
     return x**2
--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -1,10 +1,13 @@
 from mom6_regional import parabola
 
+
 def func(x):
     return x + 1
 
+
 def test_answer():
     assert func(4) == 5
+
 
 def test_parabola():
     assert parabola(2) == 4
