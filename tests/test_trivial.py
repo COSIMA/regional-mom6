ad8156a6ac17e0b5a46e574bb739a074db35a780
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-06-15 11:41:46 +1000

    allow for eps error

--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -4,4 +4,4 @@ from mom6_regional import angle_between
 
 # placeholder trivial test test
 def test_angle_between():
-    assert angle_between([1, 0, 0], [0, 1, 0], [0, 0, 1]) == np.pi / 2
+    assert np.isclose(angle_between([1, 0, 0], [0, 1, 0], [0, 0, 1]), np.pi / 2)
