2960fe794f412c68e32fe6f54ce322594264ccae
Navid C. Constantinou <navidcy@users.noreply.github.com>
2023-06-22 15:27:08 +1000

    Update test_trivial.py

--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -1,6 +1,7 @@
 import numpy as np
 from regional_mom6 import angle_between
 
+
 # placeholder trivial test test
 def test_angle_between():
     assert np.isclose(angle_between([1, 0, 0], [0, 1, 0], [0, 0, 1]), np.pi / 2)
