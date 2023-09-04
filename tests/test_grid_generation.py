11c26d6321b80c32e886345d55dabc159cda73f6
ashjbarnes <ashjbarnes97@gmail.com>
2023-09-04 12:41:08 +1000

    added basic test for rectangular hgrid

--- tests/test_grid_generation.py
+++ tests/test_grid_generation.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 from regional_mom6 import angle_between
 from regional_mom6 import quad_area
+from regional_mom6 import rectangular_hgrid
 
 
 # placeholder trivial test test
@@ -16,7 +17,7 @@ def test_angle_between(v1, v2, v3, true_angle):
     assert np.isclose(angle_between(v1, v2, v3), true_angle)
 
 
-X, Y = np.meshgrid(np.linspace(0, 90, 100), np.linspace(0, 90, 100))
+## Parameters for quad_area test
 X, Y = np.meshgrid(np.linspace(0, 90, 5), np.linspace(0, 90, 5))
 
 
@@ -30,4 +31,11 @@ def test_quad_area(lat, lon, true_area):
     assert np.isclose(np.sum(quad_area(lat, lon)), true_area)
 
 
-# what to return when pytest passes
+@pytest.mark.parametrize(
+    ("lat", "lon"),
+    [
+        (np.linspace(0, 10, 7), np.linspace(0, 10, 13)),
+    ],
+)
+def test_rectangular_hgrid(lat, lon):
+    rectangular_hgrid(lat, lon)
