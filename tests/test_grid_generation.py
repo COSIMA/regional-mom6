907f07193b04416abad913bdbc0a2d7588747b5c
ashjbarnes <ashjbarnes97@gmail.com>
2023-09-04 12:26:03 +1000

    added test for test quad area

---
+++ tests/test_grid_generation.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pytest
+from regional_mom6 import angle_between
+from regional_mom6 import quad_area
+
+
+# placeholder trivial test test
+@pytest.mark.parametrize(
+    ("v1", "v2", "v3", "true_angle"),
+    [
+        ([1, 0, 0], [0, 1, 0], [0, 0, 1], np.pi / 2),
+        ([1, 0, 0], [1, 1, 0], [0, 1, 1], np.pi / 4),
+    ],
+)
+def test_angle_between(v1, v2, v3, true_angle):
+    assert np.isclose(angle_between(v1, v2, v3), true_angle)
+
+X, Y = np.meshgrid(np.linspace(0,90,100), np.linspace(0,90,100))
+X, Y = np.meshgrid(np.linspace(0,90, 5), np.linspace(0,  90, 5))
+@pytest.mark.parametrize(
+    ("lat", "lon", "true_area"),
+    [
+        (Y,X, 0.5 * np.pi),
+    ],
+)
+def test_quad_area(lat, lon, true_area):
+
+    assert np.isclose(np.sum(quad_area(lat, lon)), true_area)
+
+# what to return when pytest passes
