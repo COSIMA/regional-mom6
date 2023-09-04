6fd32fb5c778e8c13b3722e2a9d0f68b39ceff48
Ashley Barnes <53282288+ashjbarnes@users.noreply.github.com>
2023-09-04 14:25:58 +1000

    Test hgrid generator (#62)
    * added test for test quad area
    
    * remove placeholder tests trivial
    
    * reformat with black
    
    * added basic test for rectangular hgrid
    
    * Update tests/test_grid_generation.py
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>
    
    * assert output is a dataset
    
    ---------
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- tests/test_grid_generation.py
+++ tests/test_grid_generation.py
@@ -2,6 +2,8 @@ import numpy as np
 import pytest
 from regional_mom6 import angle_between
 from regional_mom6 import quad_area
+from regional_mom6 import rectangular_hgrid
+import xarray as xr
 
 
 @pytest.mark.parametrize(
@@ -29,4 +31,12 @@ def test_quad_area(lat, lon, true_area):
     assert np.isclose(np.sum(quad_area(lat, lon)), true_area)
 
 
-# what to return when pytest passes
+# a simple test that rectangular_hgrid runs without erroring
+@pytest.mark.parametrize(
+    ("lat", "lon"),
+    [
+        (np.linspace(0, 10, 7), np.linspace(0, 10, 13)),
+    ],
+)
+def test_rectangular_hgrid(lat, lon):
+    assert isinstance(rectangular_hgrid(lat, lon), xr.Dataset)
