f876f8847b3509705a04c9f4cc8fe048846ad5b9
Ashley Barnes <53282288+ashjbarnes@users.noreply.github.com>
2023-09-04 14:03:24 +1000

    Αdd test for quad area function (#61)
    * added test for test quad area
    
    * remove placeholder tests trivial
    
    * reformat with black
    
    * Update tests/test_grid_generation.py
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>
    
    * Update tests/test_grid_generation.py
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>
    
    * Update tests/test_grid_generation.py
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>
    
    * Update tests/test_grid_generation.py
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

---
+++ tests/test_grid_generation.py
@@ -0,0 +1,32 @@
+import numpy as np
+import pytest
+from regional_mom6 import angle_between
+from regional_mom6 import quad_area
+
+
+@pytest.mark.parametrize(
+    ("v1", "v2", "v3", "true_angle"),
+    [
+        ([1, 0, 0], [0, 1, 0], [0, 0, 1], np.pi / 2),
+        ([1, 0, 0], [1, 1, 0], [0, 1, 1], np.pi / 4),
+    ],
+)
+def test_angle_between(v1, v2, v3, true_angle):
+    assert np.isclose(angle_between(v1, v2, v3), true_angle)
+
+
+# create a lat-lon mesh that covers 1/4 of the North Hemisphere
+lon, lat = np.meshgrid(np.linspace(0, 90, 5), np.linspace(0, 90, 5))
+
+
+@pytest.mark.parametrize(
+    ("lat", "lon", "true_area"),
+    [
+        (lat, lon, 0.5 * np.pi),
+    ],
+)
+def test_quad_area(lat, lon, true_area):
+    assert np.isclose(np.sum(quad_area(lat, lon)), true_area)
+
+
+# what to return when pytest passes
