4f3d9f4a1d65bd391242014ce9251a5896909de2
ashjbarnes <ashjbarnes97@gmail.com>
2024-03-13 09:09:19 +1100

    change tol from 10^-14 to -10

--- regional_mom6/regional_mom6.py
+++ regional_mom6/regional_mom6.py
@@ -440,7 +440,7 @@ def rectangular_hgrid(λ, φ):
     dλ = λ[1] - λ[0]
 
     assert (
-        np.max(np.diff(λ) - dλ) < 1e-14
+        np.max(np.diff(λ) - dλ) < 1e-10
     ), "provided array of longitudes must be uniformly spaced"
 
     # dx = R * cos(φ) * np.deg2rad(dλ) / 2
