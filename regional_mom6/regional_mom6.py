3174cbd04a8c329f23cca26d997f1db992aacff9
ashjbarnes <ashjbarnes97@gmail.com>
2024-03-26 11:36:58 +0100

    black

--- regional_mom6/regional_mom6.py
+++ regional_mom6/regional_mom6.py
@@ -175,7 +175,7 @@ def longitude_slicer(data, longitude_extent, longitude_coords):
     - Slice the ``data`` index-wise. We know that ``|longitude_extent[1] - longitude_extent[0]| / 360``
       multiplied by the number of discrete longitude points will give
       the total width of our slice, and we've already set the midpoint
-      to be the middle of the target domain. 
+      to be the middle of the target domain.
 
     - Finally re-add the right multiple of 360 so the whole domain matches
       the target.
@@ -1081,7 +1081,7 @@ class experiment:
                     np.array(self.longitude_extent)
                     + np.array(
                         [-0.5, 0.5]
-                    ),  # Hardcoded 0.5 degree longitude buffer for regridding. 
+                    ),  # Hardcoded 0.5 degree longitude buffer for regridding.
                     varnames["xh"],
                 )
             else:
@@ -1089,7 +1089,8 @@ class experiment:
                 bathy = bathy.sel(
                     {
                         varnames["xh"]: slice(
-                            self.longitude_extent[0] - 0.5, self.longitude_extent[1] + 0.5
+                            self.longitude_extent[0] - 0.5,
+                            self.longitude_extent[1] + 0.5,
                         )
                     }  # 0.5 degree longitude bufffer for regridding
                 )
