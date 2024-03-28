de6ab080fe60624582e0a69daa90586164b39e35
Navid C. Constantinou <navidcy@users.noreply.github.com>
2024-03-28 08:02:23 +0200

    Update regional_mom6/regional_mom6.py
    Co-authored-by: Angus Gibson <angus@agibson.me>

--- regional_mom6/regional_mom6.py
+++ regional_mom6/regional_mom6.py
@@ -1473,10 +1473,8 @@ class experiment:
         if surface_forcing != False:
             overwrite_run_dir = premade_rundir_path / f"{surface_forcing}_surface"
             if not overwrite_run_dir.exists():
-                available = os.listdir(premade_rundir_path)
-                # Get all of the available surface forcing types
                 available = [
-                    x for x in available if os.path.isdir(premade_rundir_path / x)
+                    x for x in premade_rundir_path.iterdir() if x.is_dir()
                 ]
                 raise ValueError(
                     f"Surface forcing {surface_forcing} not available. Please choose from {str(available)}"  ##Here print all available run directories
