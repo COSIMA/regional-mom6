264e4cd5c02cad4dcd40510bf9596f56127c6365
Andrew Kiss <31054815+aekiss@users.noreply.github.com>
2024-04-23 00:19:09 +1000

    Tweak API docstrings/comments (#158)
    * tweak API comments
    
    * few docstring enhancements
    
    * more verbose gridtype -> arakawa_grid
    
    * gridtype->arakawa_grid
    
    * format black
    
    * assert that orientation and arakawa_grid have allowed values
    
    * break ValueError messages in multiple lines for clarity
    
    * better error messages
    
    * add remark about unit conversion
    
    * units
    
    ---------
    
    Co-authored-by: Navid C. Constantinou <navidcy@users.noreply.github.com>

--- regional_mom6/utils.py
+++ regional_mom6/utils.py
@@ -5,7 +5,7 @@ def vecdot(v1, v2):
     """Return the dot product of vectors ``v1`` and ``v2``.
     ``v1`` and ``v2`` can be either numpy vectors or numpy.ndarrays
     in which case the last dimension is considered the dimension
-    over which the dot product is taken of.
+    over which the dot product is taken.
     """
     return np.sum(v1 * v2, axis=-1)
 
@@ -88,7 +88,7 @@ def quadrilateral_area(v1, v2, v3, v4):
 
 
 def latlon_to_cartesian(lat, lon, R=1):
-    """Convert latitude-longitude (in degrees) to Cartesian coordinates on
+    """Convert latitude and longitude (in degrees) to Cartesian coordinates on
     a sphere of radius ``R``. By default ``R = 1``.
 
     Args:
@@ -136,7 +136,7 @@ def quadrilateral_areas(lat, lon, R=1):
     Returns:
         numpy.array: Array with the areas of the quadrilaterals defined by the ``lat``-``lon`` grid
         provided. If the provided ``lat``, ``lon`` arrays are of dimension *m* :math:`\\times` *n*
-        then return areas array is of dimension (*m-1*) :math:`\\times` (*n-1*).
+        then returned areas array is of dimension (*m-1*) :math:`\\times` (*n-1*).
 
     Example:
 
