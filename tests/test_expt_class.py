172c3aee04a9f7c66324d8418997988fd7bf2241
ashjbarnes <ashjbarnes97@gmail.com>
2024-06-11 17:01:43 +1000

    update test function to match

--- tests/test_expt_class.py
+++ tests/test_expt_class.py
@@ -355,7 +355,7 @@ def test_ocean_forcing(
         ),
     ],
 )
-def test_rectangular_boundary(
+def test_rectangular_boundaries(
     longitude_extent,
     latitude_extent,
     date_range,
@@ -471,4 +471,4 @@ def test_rectangular_boundary(
         "tracers": {"temp": "temp", "salt": "salt"},
     }
 
-    expt.rectangular_boundary(tmp_path / "east_unprocessed", varnames, "east", 1)
+    expt.rectangular_boundaries(tmp_path, varnames)
