4d503e3280d8ad8932796b4992c1be9fff234d7e
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-03-01 09:23:02 +1100

    try tol_angle = 1e-3

--- tests/test_rotation.py
+++ tests/test_rotation.py
@@ -6,7 +6,7 @@ import xarray as xr
 import numpy as np
 import os
 
-tol_angle = 1e-4  # tolerance for angles (in degrees)
+tol_angle = 1e-3  # tolerance for angles (in degrees)
 
 
 def test_get_curvilinear_hgrid_fixture(get_curvilinear_hgrid):
