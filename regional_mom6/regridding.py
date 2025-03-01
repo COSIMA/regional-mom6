28d203619cf0d860eaccb9b20e32859be3e1124d
Navid C. Constantinou <navidcy@users.noreply.github.com>
2025-03-01 20:23:47 +1100

    regridding module docstring

--- regional_mom6/regridding.py
+++ regional_mom6/regridding.py
@@ -1,24 +1,23 @@
 """
-Custom-built helper functions to regrid the boundary conditions and ensure proper encoding for MOM6.
+Custom-built helper methods to regrid the boundary conditions and ensure proper encoding for MOM6.
 
 Steps:
 1. Initial Regridding -> Find the boundary of the ``hgrid``, and regrid the forcing variables to that boundary. Call (``initial_regridding``) and then use the ``xesmf.Regridder`` with any datasets you need.
-2. Work on some data issues
-    * Ensure temperatures are in Celsius.
-    * Fill in NaNs. This step is important for MOM6 (``fill_missing_data``) -> This diverges between
-3. For tides, we split the tides into an amplitude and a phase
-4. In some cases, here is a great place to rotate the velocities to match a curved grid (tidal_velocity), velocity is also a good place to do this.
-5. We then add the time coordinate
-6. For vars that are not just surface variables, we need to add several depth related variables
+2. Ensure temperatures are in Celsius.
+3. Fill in NaNs. This step is important for MOM6 (``fill_missing_data``) -> This diverges between
+4. For tides, we split the tides into an amplitude and a phase
+5. In some cases, here is a great place to rotate the velocities to match a curved grid (tidal_velocity), velocity is also a good place to do this.
+6. We then add the time coordinate
+7. Add several depth-related coordinates to fields that are not related to the ocean's surface (like, e.g., surface wind stress).
     * Add a `dz` variable in layer thickness
     * Some metadata issues later on
-7. Now we do up the metadata
-8. Rename variables to var_segment_num
-9. (If vertical dimension exists) Rename the vertical coordinate of the variable to nz_segment_num_var
-10. (If vertical dimension exists) Declare this new vertical coordinate as a increasing series of integers
-11. Re-add the "perpendicular" dimension
-12. ....Add  layer thickness of dz to the vertical forcings
-13. Add to encoding_dict a fill value(_FillValue) and zlib, dtype, for time, lat long, ....and each variable (no type needed though)
+8. Now we do up the metadata
+9. Rename variables to var_segment_num
+10. (If vertical dimension exists) Rename the vertical coordinate of the variable to nz_segment_num_var
+11. (If vertical dimension exists) Declare this new vertical coordinate as a increasing series of integers
+12. Re-add the "perpendicular" dimension
+13. ....Add  layer thickness of dz to the vertical forcings
+14. Add to encoding_dict a fill value(_FillValue) and zlib, dtype, for time, lat long, ....and each variable (no type needed though)
 """
 
 import xesmf as xe
