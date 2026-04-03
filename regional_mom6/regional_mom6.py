323b5adc80f218e27c9ecb6d8b6201b1ae8f932e
ashjbarnes <ashjbarnes97@gmail.com>
2026-04-03 16:52:24 +1100

    change setup_run_dir to new setup_FMS in demos

--- regional_mom6/regional_mom6.py
+++ regional_mom6/regional_mom6.py
@@ -1141,7 +1141,7 @@ class experiment:
 
         if len(self.boundaries) < 4:
             print(
-                "NOTE: the 'setup_run_directories' method does understand the less than four boundaries but be careful. Please check the MOM_input/override file carefully to reflect the number of boundaries you have, and their orientations. You should be able to find the relevant section in the MOM_input/override file by searching for 'segment_'. Ensure that the segment names match those in your inputdir/forcing folder"
+                "NOTE: the 'setup_generic' method does understand the less than four boundaries but be careful. Please check the MOM_input/override file carefully to reflect the number of boundaries you have, and their orientations. You should be able to find the relevant section in the MOM_input/override file by searching for 'segment_'. Ensure that the segment names match those in your inputdir/forcing folder"
             )
 
         if len(self.boundaries) > 4:
