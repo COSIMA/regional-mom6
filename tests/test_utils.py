50072baddbba65fcc0bdffa60a0f65cb98704871
Manish Venumuddula <80477243+manishvenu@users.noreply.github.com>
2026-09-11 15:53:35 -0600

    Make boundary/OBC forcing machinery standalone (Segment Class Expansion) (#315)
    Co-authored-by: Ashley Barnes <53282288+ashjbarnes@users.noreply.github.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,6 @@
 import numpy as np
 import pytest
 
-from regional_mom6.utils import get_edge
-
 V1 = np.zeros((2, 4, 3))
 V2 = np.zeros((2, 4, 3))
 true_V1dotV2 = np.zeros((2, 4))
@@ -15,15 +13,3 @@ for i in np.arange(2):
             V2[i, j, k] = 1.0 * np.array(i + j + k + 1)
             sum += -(2 * i - 3 * j + 2 * k) * (i + j + k + 1)
             true_V1dotV2[i, j] = float(sum)
-
-
-def test_get_edge(get_rectilinear_hgrid):
-    hgrid = get_rectilinear_hgrid
-    res = get_edge(hgrid, "north")
-    assert (res.x == hgrid.x.isel(nyp=-1)).all()
-    res = get_edge(hgrid, "south")
-    assert (res.x == hgrid.x.isel(nyp=0)).all()
-    res = get_edge(hgrid, "east")
-    assert (res.x == hgrid.x.isel(nxp=-1)).all()
-    res = get_edge(hgrid, "west")
-    assert (res.x == hgrid.x.isel(nxp=0)).all()
